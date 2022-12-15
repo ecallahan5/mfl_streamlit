e893da7d065e895afb8095c50cd7b4d6cae2b4b6
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2022-12-14 22:45:54 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 
-st.write('Hello world!')
+st.header('Playoffs!')
+
+# Example 1
+
+st.write('Wild Card 1')
+wc1 = pd.DataFrame({
+     'Team': ["Maize 'N Blue", "Moneyballers"],
+     'Win Probability': [56.37, 43.63]
+     })
+st.write(wc1)
+# Example 2
+
+st.write('Wild Card 2')
+wc2 = pd.DataFrame({
+     'Team': ["The Gurley Tates", "The Van Buren Boys"],
+     'Win Probability': [37.09, 62.91]
+     })
+st.write(wc2)
+
+
